d8737d57b55ec97c20c884d2498a38000b28633b
Ego8316 <fuusukew@gmail.com>
2026-01-07 17:00:00 +0100

    Updated script to add proper logging

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -4,18 +4,26 @@ set -e
 
 WP_DIR=/var/www/html/wordpress
 
+echo "[ftp] Bootstrapping vsftpd"
+
 mkdir -p /var/run/vsftpd/empty
 mkdir -p "$WP_DIR"
+
+echo "[ftp] Using data dir: $WP_DIR"
 
 WWW_UID=$(id -u www-data)
 WWW_GID=$(id -g www-data)
 
 if ! id "$FTP_USER" >/dev/null 2>&1; then
+	echo "[ftp] Adding user $FTP_USER (uid=$WWW_UID gid=$WWW_GID)"
 	useradd -o -u "$WWW_UID" -g "$WWW_GID" -d /var/www/html/wordpress \
 		-s /bin/bash "$FTP_USER"
 fi
-echo "${FTP_USER}:${FTP_PASS}" | chpasswd
 
+echo "${FTP_USER}:${FTP_PASS}" | chpasswd
+echo "[ftp] Password updated for $FTP_USER"
+
+echo "[ftp] Writing userlist"
 echo "$FTP_USER" > /etc/vsftpd.userlist
 
 CUR_UID=$(stat -c '%u' "$WP_DIR")
@@ -30,6 +38,9 @@ if [ -f "/vsftpd.conf.template" ]; then
 	envsubst '${FTP_PASV_ADDRESS} ${FTP_PASV_MIN} ${FTP_PASV_MAX}' \
 		< /vsftpd.conf.template > /etc/vsftpd.conf
 	rm /vsftpd.conf.template
+	echo "[ftp] Config ready (pasv ${FTP_PASV_ADDRESS}:${FTP_PASV_MIN}-${FTP_PASV_MAX})"
 fi
+
+echo "[ftp] Starting FTP server"
 
 exec vsftpd /etc/vsftpd.conf
